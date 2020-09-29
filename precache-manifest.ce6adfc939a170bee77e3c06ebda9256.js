58165c641469139ebf7dfe58e241a48ea9cf8be8
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2020-09-29 01:38:41 -0300

    Updates

---
+++ precache-manifest.ce6adfc939a170bee77e3c06ebda9256.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453f80ea103e5625168375be2baeffc8",
+    "url": "/epidemiological-report/index.html"
+  },
+  {
+    "revision": "03efcdbe8062618059f3",
+    "url": "/epidemiological-report/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "f6eba58f1d8bc383e272",
+    "url": "/epidemiological-report/static/css/main.c409bd44.chunk.css"
+  },
+  {
+    "revision": "03efcdbe8062618059f3",
+    "url": "/epidemiological-report/static/js/2.9db76e5d.chunk.js"
+  },
+  {
+    "revision": "76ea532d20a99de8e0fb888746b5c671",
+    "url": "/epidemiological-report/static/js/2.9db76e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6eba58f1d8bc383e272",
+    "url": "/epidemiological-report/static/js/main.907b6f63.chunk.js"
+  },
+  {
+    "revision": "cd8b3295d8a8024b574a",
+    "url": "/epidemiological-report/static/js/runtime-main.e0eecba1.js"
+  },
+  {
+    "revision": "1e745cca0247b6ca1c5157b286e201b1",
+    "url": "/epidemiological-report/static/media/logo.1e745cca.png"
+  }
+]);
